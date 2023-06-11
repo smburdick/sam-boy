3df8676e54fa6129b21bc6fe1c675c43c0f9618b
Sam Burdick <burdick.52@osu.edu>
2023-06-11 17:07:54 -0400

    initial files

---
+++ README.md
@@ -0,0 +1,1 @@
+Game Boy emulator in Python
---
+++ motherboard.py
@@ -0,0 +1,16 @@
+class Motherboard:
+
+    """
+        Only the motherboard may instantiate hardware components.
+    """
+    class _CPU:
+        pass
+
+    class _RAM:
+        pass
+
+    class _Cartridge:
+        pass
+
+    class _Display:
+        pass
